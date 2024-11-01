d7666d897f93ae7e1abf37467acd423fbee11b50
Rahul Saini <rahulsaini42854@gmail.com>
2024-11-01 09:26:38 +0530

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,45 @@
+"use client";
 
+import { useEffect, useState } from 'react';
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
-import styles from '@/app/ui/home.module.css';
+import { inter } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    // ...
     <>
-      <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl mx-6 md:leading-normal`}  >
+      <div className={`${inter.className} text-xl text-gray-800 md:text-3xl mx-6 md:leading-normal`}>
         <strong>Welcome to Acme.</strong> This is the example for the{' '}
         <a href="https://nextjs.org/learn/" className="text-blue-500">
           Next.js Learn Course
-        </a>
-        , brought to you by Vercel.
-        <AcmeLogo />
-      </p>
+        </a>, brought to you by Vercel.
+      </div>
+
+      {isClient && <AcmeLogo />}
+
       <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-        {/* Add Hero Images Here */}
         <Image
           src="/hero-desktop.png"
           width={1000}
           height={760}
           className="hidden md:block"
-          alt="Screenshots of the dashboard project showing desktop version"
+          alt="Dashboard project showing desktop version"
+          priority
         />
         <Image
           src="/hero-mobile.png"
           width={560}
           height={620}
           className="hidden md:block"
-          alt="Screenshots of the dashboard project showing desktop version"
+          alt="Dashboard project showing mobile version"
         />
       </div>
     </>
-    // ...
   );
 }
